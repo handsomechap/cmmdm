0e194c8df96e7dd21cf4217231c56c232305b0f7
handsomechap <mike@the-atkinsons.net>
2015-02-06 16:49:06 +0000

    Update installcmmdm.sh

--- installcmmdm.sh
+++ installcmmdm.sh
@@ -13,9 +13,12 @@
 # ./installcmmdm.sh
 ##################################################
 
-# 1 - create suspended domain page to redirect to
+# 1 - create suspended domain page to redirect to, set your own if you want to
+CMMDMDIR='/home/nginx/domains/cmmdm'
+mkdir -p $CMMDMDIR/suspendedpage
+touch $CMMDMDIR/suspendedpage/index.html
+chown -R nginx $CMMDMDIR 
 
-SUSPDIR='/home/nginx/domains/suspendedpage'
-
-# add aliases for suspend/unsuspend domains
-
+# 2 -add aliases for suspend/unsuspend domains
+echo 'bash suspend.sh' >/usr/bin/cmmdmsuspend
+echo 'bash unsuspend.sh' >/usr/bin/cmmdmunsuspend
