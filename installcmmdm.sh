282e3e43131dd6e3562d2a96e51c909682abb994
handsomechap <mike@the-atkinsons.net>
2015-02-07 08:02:14 +0000

    Update installcmmdm.sh

--- installcmmdm.sh
+++ installcmmdm.sh
@@ -13,12 +13,25 @@
 # ./installcmmdm.sh
 ##################################################
 
-# 1 - create suspended domain page to redirect to, set your own if you want to
+# - welcome message
+echo "starting cmmdm installation"
+
+# - download cmmdm.sh
+wget -cnv --no-check-certificate https://raw.github.com/cmmdm/cmmdm.sh -O /usr/bin/cmmdm --tries=3
+ERROR=$?
+if [[ "$ERROR" != '0' ]]; then
+echo "Error: /usr/bin/wp download failed."
+exit $ERROR
+else
+echo "Download done."
+fi
+
+# - create suspended domain page to redirect to, set your own if you want to
 CMMDMDIR='/home/nginx/domains/cmmdm'
 mkdir -p $CMMDMDIR/suspendedpage
 touch $CMMDMDIR/suspendedpage/index.html
 chown -R nginx $CMMDMDIR 
 
-# 2 -add aliases for suspend/unsuspend domains
-echo 'bash suspend.sh' >/usr/bin/cmmdmsuspend
-echo 'bash unsuspend.sh' >/usr/bin/cmmdmunsuspend
+# - add alias for interacting with cmmdm
+echo 'bash cmmdm.sh' >/usr/bin/cmmdm
+
