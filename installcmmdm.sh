19747ba364b6c0f265d90c132ae65be30084cefc
handsomechap <mike@the-atkinsons.net>
2015-02-07 08:25:47 +0000

    Update installcmmdm.sh

--- installcmmdm.sh
+++ installcmmdm.sh
@@ -23,10 +23,10 @@ echo "starting cmmdm installation"
 wget -cnv --no-check-certificate https://raw.githubusercontent.com/handsomechap/cmmdm/master/cmmdm.sh -O /root/tools/cmmdm.sh --tries=3
 ERROR=$?
 if [[ "$ERROR" != '0' ]]; then
-echo "Error: cmmdm.sh download failed."
+echo "Error: cmmdm.sh download from github failed."
 exit $ERROR
 else
-echo "Download cmmdm successful."
+echo "Okay: cmmdm.sh download from github successful."
 fi
 # - modify script permissions to be runnable
 chmod 0700 /root/tools/cmmdm.sh
@@ -34,6 +34,7 @@ chmod 0700 /root/tools/cmmdm.sh
 
 ##########################
 # - create suspended domain page to redirect to, set your own if you want to
+echo "Creating suspension page for redirects"
 CMMDMDIR='/home/nginx/domains/cmmdm'
 mkdir -p $CMMDMDIR/suspendedpage
 touch $CMMDMDIR/suspendedpage/index.html
@@ -42,5 +43,6 @@ chown -R nginx $CMMDMDIR
 
 ##########################
 # - add alias for interacting with cmmdm
+echo "Creating new command: cmmdm"
 echo 'bash /root/tools/cmmdm.sh' >/usr/bin/cmmdm
 ##########################
