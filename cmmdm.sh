7d0f61b29f03356c9aa77e8ff365c86e28a49c0e
handsomechap <mike@the-atkinsons.net>
2015-02-07 08:03:48 +0000

    Update cmmdm.sh

--- cmmdm.sh
+++ cmmdm.sh
@@ -1,1 +1,10 @@
-#######################################
+#!/bin/bash
+##################################################
+# cmmdm script
+# Michael Atkinson
+# v. 0.1
+##################################################
+# https://github.com/handsomechap/cmmdm
+# set of commandline tools to enable easier domain
+# management within centminmod hosting environment
+##################################################
