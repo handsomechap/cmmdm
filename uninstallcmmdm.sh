7a676da5202d1251a95460dcb58778c683a4d476
handsomechap <mike@the-atkinsons.net>
2015-02-07 09:06:21 +0000

    Create uninstallcmmdm.sh

---
+++ uninstallcmmdm.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+##################################################
+# cmmdm uninstaller
+# Michael Atkinson
+# v. 0.1
+##################################################
+# https://github.com/handsomechap/cmmdm
+# set of commandline tools to enable easier domain
+# management within centminmod hosting environment
+##################################################
+
+##########################
+echo "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
+echo "performing uninstall routine now"
+echo "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
+##########################
+
+##########################
+# - script installation completed
+echo "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
+echo "Okay: cmmdm.sh Installation Completed."
+echo "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
+##########################
